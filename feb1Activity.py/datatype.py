13dfd10b2d81c0bedd363a87cd2a4310d98bee91
jtrap2 <158080145+jtrap2@users.noreply.github.com>
2024-02-02 17:49:56 +0000

    data lesson completed

---
+++ feb1Activity.py/datatype.py
@@ -0,0 +1,10 @@
+# funtions are just instructions that do a job
+#take in user data
+name= input('what is your name')
+age= input("what is your age")
+location= input("where do you live")
+# puts out data.
+
+print(name)
+print(age)
+print(location)
