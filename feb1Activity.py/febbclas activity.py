9e6ba1384d5db70625c16d9abe73315c3e4a9083
jtrap2 <158080145+jtrap2@users.noreply.github.com>
2024-02-08 17:31:36 +0000

    feb8dailyasigment

---
+++ feb1Activity.py/febbclas activity.py
@@ -0,0 +1,13 @@
+#camal case some defenition 
+
+
+bankaccount = 100.00
+deposit = float(input('how much would you like to deposit'))
+print('your bank total')
+print(bankaccount+ deposit)
+#string()
+str()
+
+int()
+
+float()
