9b6464fbb1728cd8209d76d835e87b01583e65d8
jtrap2 <158080145+jtrap2@users.noreply.github.com>
2024-02-14 17:50:36 +0000

    feb.14.py

---
+++ feb14.py
@@ -0,0 +1,3 @@
+a function that you made using diffretn ConnectionAbortedError
+a built in function we been using is input
+input
---
+++ feb1Activity.py/feb.14.py
@@ -0,0 +1,17 @@
+def penutbutterandjelly():
+    print('ingedients:penutbutter, jelly, 2 dslices of bread')
+    print('step 2 spread penut butter on one peice of bread')
+    print('step 3 spread jelly on the other peice of bread')
+    print(' step 4 put both sides together with th penut butter and jelly facing each other')
+    penutbutterandjelly()
+
+    def cheesesteak():
+        print('Ingediants: steak, salt,  pepper, choice of chese,chopped oions,  hoagie roll, fried onion, mayo, ketchup ')
+        print('cookware: spatula, frying pan,oil, knife')
+        print('step 1 turn on the  stovetop to 350 degrees then add oil evenly til oil is hot in 3 minutes ')
+        print('step 2 once/pan is hot apply steak to pan. season with salt and pepper. fully cook the meat')
+        print('step 3 sautee chopped onions with glazed/ golden brown.')
+        print('sttep 4 add onions to steak')
+        print('step 5. cut open hoagie bread in half , add cheeese, mayo, ketchup ')
+        cheesteak()
+
