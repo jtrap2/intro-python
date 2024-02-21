e25c92fce02b130adcedfe248d6b1ce8d48e19a8
jtrap2 <158080145+jtrap2@users.noreply.github.com>
2024-02-21 17:04:17 +0000

    feb21.py

---
+++ feb1Activity.py/dailywarmupfeb20.py
@@ -0,0 +1,33 @@
+
+laundry=['socks, draws,pant, shirts,hats']
+print(len(laundry))
+# 2. create a function that will only
+#print the item in your laundry list at index 3
+def laundryindex():
+    print(laundry[3])
+    print('hats')
+    laundryindex()
+
+
+def reverse():
+    username = input('what is your name?')[::-1]
+    print(username)
+ 
+reverse()
+
+def cheesecake():
+    print("these are the steps for makign a chesse cake....")
+    chesecake()
+    varia
+
+
+
+
+
+
+
+    def math(number):
+        result= number + 5
+        print(result)
+
+        math(5)
---
+++ feb1Activity.py/feb21.py
@@ -0,0 +1,13 @@
+#is a form of argumentative coversation between individuals, based on asking and answering questions.
+#to derstand what your doing
+
+
+#video 2
+is and organazitation that  makes money on collecting personal imformation
+represent opportunities for new revenue streams.
+    
+
+    video 3
+#structural raciism means some rules and system in our society treatpeople unfairly based on their race, making it harder for certain groupps to succed.(An example of structural racism i like having a game  where some player start with more point than others not becuase they  are better but becuase of there color of ther skin)
+#becuase certain groups might not know the algorithim
+#so the code can work with us to
