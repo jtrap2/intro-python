2bac4d01c1784026760dff9c905a87b84d33f49f
jtrap2 <158080145+jtrap2@users.noreply.github.com>
2024-02-26 17:12:01 +0000

    feb26.py

---
+++ feb1Activity.py/dailyasigment 26.py
@@ -0,0 +1,61 @@
+#you would use the print()function
+#you eould usr input function
+
+
+#complexity is the measure  of how rsources intensive
+#a program can be
+abstraction is the process of hiding/masking complexity
+#this occurs when you are not using the 
+#proper graammar/launguage role of python
+
+
+#A VARIBLE IS A CONTAINER FOR DATA
+#write a conde that prints you naame
+def whatisyourname():
+    print('jaden')
+    whatisyourname()
+
+    #function defenition is instruction that will only run when called
+    #function call executed the instruction
+#= // single equal  is asighment
+carrerer='enginer'
+score = 60
+
+# == //double equal
+'comparison operators. we use to compare values'
+100>20
+100 == 100
+
+# != // excamation equal
+'comparison operators we use it to compare if  values are not the same
+
+
+var1 =1
+var =2
+var ='3'
+print(var1 + var2 + var3)
+print(listone==listtwo)
+'true bothlist are the same'
+
+create a lis of stings the string can be items found in your bage
+bookbagitems=['pencils, paper, noyebook, foldeer']
+create a lidt of floats
+bookbsgitemss=['1.00, 2.00, 3.00']
+
+#whats the 3 naming formats we use for variables
+'camal case'-'
+'first letter is lowercase but every new word  is uppercase"
+'pascal case'
+'every word is uppercased'
+'snake case'
+'every word is sperated with and underscore'
+
+
+where would you use comparison
+'comparemembership--. check if somone has bronze, silver, or gold tie memberhsip.'
+membtier = 'gold'
+tierone = 'gold'
+tiertwo='silver'
+tier3='bronze'
+meertier != tierone
+membertier == tier one
