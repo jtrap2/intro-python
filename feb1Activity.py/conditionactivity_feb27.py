2ec542494328599b1b48a1bcc3c057c3457ae322
jtrap2 <158080145+jtrap2@users.noreply.github.com>
2024-03-01 17:40:43 +0000

    daily assigmnet

--- feb1Activity.py/conditionactivity_feb27.py
+++ feb1Activity.py/conditionactivity_feb27.py
@@ -1,6 +1,8 @@
 def access_control(user_input):
-    correctpasscode = "get_this_money"
-    if user_input == correct_passcode
-    return "access granted. welcome!"
-else:
-    return "acces denied. Incorrect "
+    correct_passcode = "get_this_money"
+    if user_input == correct_passcode:
+        print("access granted. welcome!")
+    else:
+        print("acces denied. Incorrect ")
+
+access_control("get_this_money")
