3977490a18cb76772ccf55aa66cb2bacc17b216d
jtrap2 <158080145+jtrap2@users.noreply.github.com>
2024-03-07 16:35:36 +0000

    async work issues with git exempt Ian

---
+++ asyc march5.py
@@ -0,0 +1,65 @@
+def elevator_system():
+    building_floors = {
+        'M': 'lobby',
+        'B': 'basement',
+        'R': 'rooftop',
+        1: 'gym',
+        2: 'restaurant',
+        3: 'workspace',
+        4: 'living quarters'
+    }
+
+    user_input = input("Enter the floor number (M, B, R, 1, 2, 3, 4): ")
+
+    if user_input in building_floors:
+        print(f"Going to {building_floors[user_input]}!")
+    else:
+        print("Oops! That floor doesn't exist. Please enter another number or letter and try again.")
+
+
+#elevator_system()
+wrong code
+def check_roller_coaster_eligibility(age, height):
+    if age >= 14 and height >= 5.2:
+        print("You are eligible to ride Roller Coaster 1.")
+        elig
+   
+       print("You are eligible to ride Roller Coaster 3.")
+    elif age< and height<5.2:
+      "You are eligible to ride Roller Coaster 3.")  
+    else:
+        print("Error: Please enter your information correctly. You may not meet the requirements for any roller coaster.")
+
+check_roller_coaster_eligibility(2,3)
+
+def RollerCoaster (Height, age):
+
+if Height <= 5.1 and age <= 14:
+
+('print you are able to ride roller coaster number 3.')
+
+elif Height « 5.2 and age >= 14:
+
+print('you are able to ride roller coaster number 2')
+
+elif Height >= 5.2 and age = 14:
+
+else:
+
+print(you are able to ride roller coaster number 1')
+
+print 'If your info does not make you eligible to ride any of the roller coaters there could be an error please ask a local
+
+Rollercoaster (5.5,14)
+
+
+
+
+
+
+
+
+# daily work
+# def
+
+#elif means if and or else
