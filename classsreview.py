8a64090efd03a07762224d18f4d0afdd1751eb9c
jtrap2 <158080145+jtrap2@users.noreply.github.com>
2024-03-10 18:23:06 +0000

    weekend work

---
+++ classsreview.py
@@ -0,0 +1,38 @@
+# pytho reveiw sheet
+
+
+#vocabulary
+
+# 1. programs- instructions for computers to peform sertain task
+
+#2. programing language- a set of instructions that aa computer can execute in ordr to peform a specefic task or acheive a particular goal. it is a sequence of command s statements and algorithim writtem in a programing language that guides A a computer hardware to carry put desired operations
+# a computer program  can simpy some complex problems
+# a computer program can automate certain processes
+#a computer program can be reused andmodified
+
+# 2. programing launguage- diffrent ways we can write instructions for computers
+
+# 3. syntax-  the formating rules if a laugauge  IN THIS CASE A PORGRAMING
+
+# THIS HOW YOU WRITE A FUNCTION 
+ def myfunction()
+
+ print('here are my instrucctions...')
+# this is how you write a function in javascript
+function myfunction(){
+ conaole.log('here are my instructions...')
+}
+
+public class method{
+ static void myfunction(){
+  system.out.println('here are ny instruction...')
+}
+
+
+
+data type - the basic building blocks for a program laguage
+# basically our programing laguages alphabet
+
+5# variablles - containers for data (datatypes.)
+
+additional note- when we are writting programs all we are doing is talking to the computer and giving it instructions to complete a task
---
+++ feb1Activity.py/pythonWeekendActivity.py
@@ -0,0 +1,81 @@
+#1. t hings that i notice is we have to make a list
+
+def check_vip_status(guest_name):
+    VIP_Guests=["Bill", "Mary", "Dante", "Stacy", "Harry", "Ashley"] 
+    if guest_name in VIP_Guests:
+        return f"Welcome, {guest_name}! You may enter the VIP section."
+    else:
+        return f"Sorry, {guest_name}. Please proceed to the general admission section."
+    check_vip_status()
+
+
+
+# key sentences that helped me figure out the code
+#The store would like your function to ask
+# users to enter their name, their membership tier, 
+   
+def caculatee_discount()
+name = input("enter your name: ")
+tier = input("enter your mebership tier(gold/silver/bronze):")
+item = input("enter the item you are buying")
+price= float(input("enter the items price: "))
+
+discoutn_percentage = 0
+if tier == "gold":
+    discoutn_percentage = 0.25
+elif tier == "silver":
+    discoutn_percentage = 0.15
+elif tier == "bronze":
+    discoutn_percentage = 0.05
+
+print("congradulations{} her is your discounted price")
+
+
+
+
+def unknownFunction_A(data1, data2):
+    newData = data2 + 10
+    print(f"hello, {data1}. Welcome to Boys Latin.")
+    print("by solving this, you have gotten"+ data2 "points added to your grade")
+
+
+    # def unknownFunction_A(data1, data2):
+    #this lined defines a function name 'unkownfunction_a that takes two parameters'data1' and 'data2'
+    #newData = data2 + 10
+    # this line create a new variable 'newdata'and assigns it to the value of 'data2' +10
+    #print(f"hello, {data1}. Welcome to Boys Latin.")
+    # this line prints a string welcoming the user with the value'data' including in the greeting
+    #print("by solving this, you have gotten"+ data2 "points added to your grade")
+    #print("by solving this, you have gotten"+ data2 "points added to your grade")
+    # this line has a sytax error it is missing abother'+' operator
+      print("by solving this, you have gotten " + str(data2) + " points added to your grade")
+    #that is what is suppose to look like and  the code prints a message saying saying that the user has earned certain points added to their grade by solving a proble
+
+
+
+    def unknownFunction_B(data1, data2):
+    #this line defines a function name'unknownfunction_b' that takes two pramater'data1 and 'data2'
+    if data1 == "btp@gmail.com" and data2 == "abc123":
+    #THis lineThis line starts an if statement checking if data1 is equal to "btp@gmail.com" and if data2 is equal to "abc123". If both conditions are true, the code block inside the if statement is executed.
+	    print("Welcome, access granted.")
+    #if the conditions in the 'if' statements are true this line print a wolcome message showing that acces is granted
+    elif data1 != "btp@gmail.com" or data2 != "abc123":
+    #If the conditions in the initial if statement are not met, this line checks if either data1 is not equal to "btp@gmail.com" or data2 is not equal to "abc123". If this condition is true, the code block inside the elif statement is executed.
+ 	    print("Sorry, your information is incorrect")
+    #if the conditions in the 'elif' statement are true this line prints a message showing that the provided imformation is correct
+    else:
+    # if none of the following conditons are true this line shows the 'else' statment showingtha the code block inside it will be executed if non of the previous conditons are met
+        print(" Error: somthing went wrong. Try again")
+     #this line printst and error message showing something went wrong and suggest trying agin
+
+# i think that technology influenced people in a good way . I think that is has influenced people in a good way becuase it gives us shortcuts on many thing that might take us a long time for us humans to do  if we where to dod it are selfes. i
+# for example without technology we wouldnt have  social media are the internet wich would make it harder to communicate  with others
+# that could be bad becuaser there alot of influencers and famous people out the who use social media as outlet to inspire peope and show there acomplisments
+#also technology influence people in a good way becuase  without technolgy we would habe not have the mediacl advancments we have now like covid 19 shot flu shots and more
+
+
+
+# technology helps people learn becuase people learn diffrent and   with technology you can see diffrent people point of view and we can learn from that.
+#not to mention technology make learning way easir with stuff like the computer to look up stuff  that we might not now in second, caculator so we dont have to figur out long quations and many more.
+# technology helps with school becuas without it would have to write and read  for hour and this may be a personal matter but my hand cramps up alot when im writing so it helps my hand and readin books are cool and all but visual representation has shown to help alot of people learn thigngs
+#lastly i think that technology helps people becuase it helps student and teacher communicated for example if you missing a assignment instead of trying to find ther all the time to keeep on asking tfor work you can email her wich is way more effecient
